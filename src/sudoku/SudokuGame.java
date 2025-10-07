bd08176c477b53235b3e2e0e29ffbf3b4cf510ba
Darren <77473060+Phaedrem@users.noreply.github.com>
2025-10-07 13:42:19 -0500

    Added two additional strings for increased difficulty. Will allow user to select difficulty in future update.

--- src/sudoku/SudokuGame.java
+++ src/sudoku/SudokuGame.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class SudokuGame {
 
-    public static final String EASY = 
+    private static final String EASY =
         "003020600" +
         "900305001" +
         "001806400" +
@@ -14,6 +14,28 @@ public class SudokuGame {
         "002609500" +
         "800203009" +
         "005010300";
+
+    private static final String MEDIUM =
+        "200080300" +
+        "060070084" +
+        "030500209" +
+        "000105408" +
+        "000000000" +
+        "402706000" +
+        "301007040" +
+        "720040060" +
+        "004010003";
+
+    private static final String HARD =
+        "000000907" +
+        "000420180" +
+        "000705026" +
+        "100904000" +
+        "050000040" +
+        "000507009" +
+        "920108000" +
+        "034059000" +
+        "507000000";
 
     public static void main(String[] args) {
         // 1. Create a board from the string
