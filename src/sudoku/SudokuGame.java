bd4e9d1c17c587eb7e18480b76362e3c63bc3455
Darren <77473060+Phaedrem@users.noreply.github.com>
2025-10-07 13:01:48 -0500

    Added set option to accept row, column, value after set and try to add to the cell

--- src/sudoku/SudokuGame.java
+++ src/sudoku/SudokuGame.java
@@ -42,8 +42,26 @@ public class SudokuGame {
                             (more commands otw)
                     """);
                 }
-                case "print" -> {
-                    Renderer.print(board);
+                case "print" -> { Renderer.print(board); }
+                case "set" -> {
+                    String[] parts = in.nextLine().trim().split("\\s+");
+                    if(parts.length < 3) {
+                        System.out.println("Set needs 3 parts: row, column, and value (e.g., set 1 3 5).");
+                        break;
+                    }
+                    try {
+                        int r = Integer.parseInt(parts[0]) - 1;
+                        int c = Integer.parseInt(parts[1]) - 1;
+                        int v = Integer.parseInt(parts[2]);
+                        if(board.isValidPlacement(r, c, v)){
+                            board.cell(r, c).setValue(v);
+                        }else{
+                            System.out.println("Invalid Placement");
+                        }
+                        Renderer.print(board);
+                    } catch (NumberFormatException ex) {
+                        System.out.println("Set uses intergers: set row column value (e.g., set 1 3 5)");
+                    }
                 }
                 case "quit" -> {
                     System.out.println("Goodbye!");
