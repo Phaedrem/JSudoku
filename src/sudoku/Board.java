2d46eb9f44ee78009150975349667a2361c944ff
Darren <77473060+Phaedrem@users.noreply.github.com>
2025-10-06 23:49:02 -0500

    Fleshed out Board methods to test current functionality

--- src/sudoku/Board.java
+++ src/sudoku/Board.java
@@ -3,11 +3,11 @@ package sudoku;
 public class Board {
     private final Cell[][] grid = new Cell[9][9];
 
-    public Board(int[][] start) { // Initalize board
-        for (int r = 0; r < 9; r++){
-            for (int c = 0; c < 9; r++){
+    public Board(int[][] start) { // Initalize the board
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
                 int v = start[r][c];
-                grid[r][c] = new Cell(v, v != 0);
+                grid[r][c] = new Cell(v, v != 0); // given if nonzero
             }
         }
     }
@@ -20,17 +20,73 @@ public class Board {
 
     public boolean isValidPlacement(int r, int c, int v) { // Check if provided addition is valid
         boolean valid = true;
-        if(!inBounds(r, c) || v < 1 || v > 9) { return false; } 
+        if(!inBounds(r, c) || v < 1 || v > 9) { valid = false; } 
+        for(int i = 0; i < 9; i++){ // Checks if provided number is already in the row or column it's attemping to be added to
+            if (grid[r][i].getValue() == v) {
+                valid = false;
+                break;
+            }
+            if (grid[i][c].getValue() == v) {
+                valid = false;
+                break;
+            }
+        }
+        int br = (r / 3) * 3, bc = (c/3) * 3;
+        for (int rr = br; rr < br + 3; rr++){ // Check the 3x3 box for existing value
+            for (int cc = bc; cc < bc + 3; cc++){
+                if (grid[rr][cc].getValue() == v){
+                    valid = false;
+                    break;
+                }
+            }
+        }
         return valid;
     }
 
-    public boolean trySet(int r, int c, int v){ // Try to place a number
+    public boolean trySet(int r, int c, int v){ // Sets value if that cell isn't already given and the placement is valid
         boolean success = false;
+        if (inBounds(r, c)){
+            var cell = grid[r][c];
+            if(!cell.isGiven()){
+                if(isValidPlacement(r, c, v)){
+                    cell.setValue(v);
+                    success = true;
+                }
+            }
+        }
         return success; 
     }
 
+    public boolean tryClear(int r, int c){
+        boolean success = false;
+        if(inBounds(r, c)){
+            var cell = grid[r][c];
+            if(!cell.isGiven()){
+                cell.clearValue();
+                success = true;
+            }
+        }
+        return success;
+    }
+
     public boolean isSolved() { // Check if board is solved
-        boolean solved = false;
+        boolean solved = true;
+        for (int r = 0; r < 9; r++){
+            for (int c = 0; c < 9; c++){
+                int v = grid[r][c].getValue();
+                if (v == 0){
+                    solved = false;
+                    break;
+                }
+                grid[r][c].setValue(0); // Setting every point to 0 and then cylce through each cell to verify correct solution
+                boolean ok = isValidPlacement(r, c, v);
+                grid[r][c].setValue(v);
+                if(!ok) {
+                    solved = false;
+                    break;
+                }
+            }
+        }
         return solved;
     }
 
