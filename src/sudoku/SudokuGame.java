fc88951db5ba35e3886dff03215bced43e2d7470
Darren <77473060+Phaedrem@users.noreply.github.com>
2025-10-07 00:02:22 -0500

    Created an initial board and started intro for prompts

--- src/sudoku/SudokuGame.java
+++ src/sudoku/SudokuGame.java
@@ -1,7 +1,32 @@
 package sudoku;
 
+import java.util.Scanner;
+
 public class SudokuGame {
-    public static void main(String[] args){
-        System.out.println("Welcome to Sudoku!");
+
+    public static final String EASY = 
+        "003020600" +
+        "900305001" +
+        "001806400" +
+        "008102900" +
+        "700000008" +
+        "006708200" +
+        "002609500" +
+        "800203009" +
+        "005010300";
+
+    public static void main(String[] args) {
+        // 1. Create a board from the string
+        Board board = Board.fromString(EASY);
+
+        // 2. Print a welcome message
+        System.out.println("Current board:");
+
+        // 3. Render the board to terminal
+        Renderer.print(board);
+
+        // 4. Placeholder for looping input
+        System.out.println("Type 'help' for commands");
+
     }
 }
