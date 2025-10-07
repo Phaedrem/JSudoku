45ac533ba2101d38635483cb1c9c3ba396c91a5c
Darren <77473060+Phaedrem@users.noreply.github.com>
2025-10-06 22:41:33 -0500

    Created first attempt at method to print the board in terminal

--- src/sudoku/Renderer.java
+++ src/sudoku/Renderer.java
@@ -2,4 +2,17 @@ package sudoku;
 
 public class Renderer {
     
+    public static void print(Board b){
+        String sep = "+-------+-------+-------+";
+        for(int r = 0; r < 9; r++){
+            if (r % 3 == 0) System.out.println(sep);
+            for(int c = 0; c < 9; c++){
+                if (c % 3 == 0) System.out.print("| ");
+                int v = b.cell(r,c).getValue();
+                System.out.print((v == 0 ? "." : (v + " ")));
+            }
+            System.out.println("|");
+        }
+        System.out.println(sep);
+    }
 }
