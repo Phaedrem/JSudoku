800c0118090fc61b2a9058b29d5de8555b0ca686
Darren <77473060+Phaedrem@users.noreply.github.com>
2025-10-09 18:23:13 -0500

    Updated to label rows and columns on print

--- src/sudoku/Renderer.java
+++ src/sudoku/Renderer.java
@@ -3,9 +3,11 @@ package sudoku;
 public class Renderer {
     
     public static void print(Board b){
-        String sep = "+-------+-------+-------+";
+        String sep = " +-------+-------+-------+";
+        System.out.println("   1 2 3   4 5 6   7 8 9");
         for(int r = 0; r < 9; r++){
             if (r % 3 == 0) System.out.println(sep);
+            System.out.print(r+1);
             for(int c = 0; c < 9; c++){
                 if (c % 3 == 0) System.out.print("| ");
                 int v = b.cell(r,c).getValue();
