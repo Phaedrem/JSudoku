22eef586fadf56a826871a33373c68e63620b22e
Darren <77473060+Phaedrem@users.noreply.github.com>
2025-10-09 14:25:12 -0500

    Updated solvedBoard method to call BoardUtils, added countRec and countSolutions to check if board has multiple solutions

--- src/sudoku/Solver.java
+++ src/sudoku/Solver.java
@@ -1,6 +1,10 @@
 package sudoku;
 
+import util.BoardUtils;
+
 public class Solver {
+
+    int numSolutions = 0;
 
     public static boolean solve(Board board){
         int[] pos = findEmpty(board);
@@ -9,7 +13,6 @@ public class Solver {
         }
         int r = pos[0];
         int c = pos[1];
-
         for (int v = 1; v <= 9; v++){
             if (board.isValidPlacement(r, c, v)){
                 board.cell(r, c).setValue(v);
@@ -20,7 +23,6 @@ public class Solver {
             }
         }
         return false;
-
     }
     
     private static int[] findEmpty(Board board){
@@ -35,13 +37,7 @@ public class Solver {
     }
 
     public static Board solvedBoard (Board orignal){
-        int[][] grid = new int[9][9];
-        for (int r = 0; r < 9; r++){
-            for (int c = 0; c < 9; c++){
-                grid[r][c] = orignal.cell(r, c).getValue();
-            }
-        }
-        Board copy = new Board(grid);
+        Board copy = BoardUtils.copy(orignal);
         if(solve(copy)){
             return copy;
         }else {
@@ -49,4 +45,28 @@ public class Solver {
         }
     }
 
+    private static int countRec(Board b, int limit){
+        int[] pos = findEmpty(b);
+        if (pos == null) return 1;
+        int r = pos[0];
+        int c = pos[1];
+        int total = 0;
+        for (int v = 1; v <= 9; v++){
+            if (b.isValidPlacement(r, c, v)){
+                b.cell(r,c).setValue(v);
+                total += countSolutions(b, limit - total);
+                if (total >= limit) {
+                    b.cell(r, c).setValue(0);
+                    return total;
+                }b.cell(r, c).setValue(0);
+            }
+        }
+        return total;
+    }
+
+    public static int countSolutions(Board original, int limit){
+        if (limit < 1) limit = 1;
+        Board copy = BoardUtils.copy(original);
+        return countRec(copy, limit);
+    }
 }
