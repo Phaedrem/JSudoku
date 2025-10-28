b08d24c8c22db2707c4689b90dfbdbb0a176ebcc
Darren <77473060+Phaedrem@users.noreply.github.com>
2025-10-28 15:29:08 -0500

    Built applyBoxBoarders method and called in constructor to display boarders around cells and 3x3 boxes

--- src/ui/CellView.java
+++ src/ui/CellView.java
@@ -1,7 +1,8 @@
 package ui;
 
 import java.awt.Dimension;
-import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.border.*;
 
 public class CellView extends JPanel{
     private final int row, col;
@@ -11,8 +12,21 @@ public class CellView extends JPanel{
         this.col = col;
         setOpaque(true);
         setPreferredSize((new Dimension(60, 60)));
+        applyBoxBoarders();
     }
     
     public int row() {return row; }
     public int col() {return col; }
+
+    private void applyBoxBoarders() {
+        int top = 1, left = 1, bottom = 1, right = 1;
+
+        if (row % 3 == 0) top = 3;
+        if (col % 3 == 0) left = 3;
+        if (row == 8) bottom = 3;
+        if (col == 8) right = 3;
+
+        Border b = new MatteBorder(top, left, bottom, right, getForeground());
+        setBorder(b);
+    }
 }
