766bffbb4ce810ef7c1ea7317f4ebe1dbb7af361
Darren <77473060+Phaedrem@users.noreply.github.com>
2025-11-03 14:08:14 -0600

    Added main menu bar with file and settings

--- src/ui/SudokuFrame.java
+++ src/ui/SudokuFrame.java
@@ -11,8 +11,33 @@ public class SudokuFrame extends JFrame {
         setLayout(new BorderLayout());
 
         add(new BoardPanel(board), BorderLayout.CENTER);
+        setJMenuBar(createMenuBar());
 
         setSize(600, 650);
         setLocationRelativeTo(null);
+    }
+
+    private JMenuBar createMenuBar(){
+        JMenuBar bar = new JMenuBar();
+        JMenu filMenu = new JMenu("File");
+        JMenuItem newItem = new JMenuItem("New Game");
+        JMenuItem saveItem = new JMenuItem("Save Game");
+        JMenuItem loadItem = new JMenuItem("Load Game");
+        JMenuItem exitItem = new JMenuItem("Exit");
+
+        filMenu.add(newItem);
+        filMenu.add(saveItem);
+        filMenu.add(loadItem);
+        filMenu.addSeparator();
+        filMenu.add(exitItem);
+
+        JMenu viewMenu = new JMenu("Settings");
+        JMenuItem settingsItem = new JMenuItem("Colors");
+        viewMenu.add(settingsItem);
+
+        bar.add(filMenu);
+        bar.add(viewMenu);
+
+        return bar;
     }
 }
