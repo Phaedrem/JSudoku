b607fb09554c5a46e68a2a12d885fbc085d58ce6
Darren <77473060+Phaedrem@users.noreply.github.com>
2025-11-03 15:31:37 -0600

    Created New Game submenu which populates a new game based on chosen difficulty

--- src/ui/SudokuFrame.java
+++ src/ui/SudokuFrame.java
@@ -2,15 +2,21 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+
+import sudoku.Board;
+import sudoku.BoardFacade;
 import sudoku.BoardView;
 
 public class SudokuFrame extends JFrame {
+    private BoardPanel boardPanel;
+
     public SudokuFrame(BoardView board) {
         super("JSudoku");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        add(new BoardPanel(board), BorderLayout.CENTER);
+        setBoardView(board);
         setJMenuBar(createMenuBar());
 
         setSize(600, 650);
@@ -20,12 +26,18 @@ public class SudokuFrame extends JFrame {
     private JMenuBar createMenuBar(){
         JMenuBar bar = new JMenuBar();
         JMenu filMenu = new JMenu("File");
-        JMenuItem newItem = new JMenuItem("New Game");
+        JMenu newMenu = new JMenu("New Game");
+        String[] diffs = {"Easy", "Medium", "Hard", "Multi", "Impossible"};
+        for (String d : diffs){
+            JMenuItem item = new JMenuItem(d);
+            item.addActionListener(this::startNewPuzzle);
+            newMenu.add(item);
+        }
+        filMenu.add(newMenu);
         JMenuItem saveItem = new JMenuItem("Save Game");
         JMenuItem loadItem = new JMenuItem("Load Game");
         JMenuItem exitItem = new JMenuItem("Exit");
 
-        filMenu.add(newItem);
         filMenu.add(saveItem);
         filMenu.add(loadItem);
         filMenu.addSeparator();
@@ -39,5 +51,23 @@ public class SudokuFrame extends JFrame {
         bar.add(viewMenu);
 
         return bar;
+    }
+
+    private void startNewPuzzle(ActionEvent e){
+        String label = ((JMenuItem) e.getSource()).getText().toLowerCase();
+        String seed = sudoku.Seeds.BY_NAME.get(label);
+        Board core = Board.fromString(seed);
+        BoardView view = new BoardFacade(core);
+        setBoardView(view);
+    }
+
+    private void setBoardView(BoardView view){
+        if (boardPanel != null){
+            getContentPane().remove(boardPanel);
+        }
+        boardPanel = new BoardPanel(view);
+        getContentPane().add(boardPanel, BorderLayout.CENTER);
+        revalidate();
+        repaint();
     }
 }
